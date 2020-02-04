1a2f5cfb47f3df97619fe758bbc89d74fb01d97c
Domagoj Salkić <48859348+domagojsalkic@users.noreply.github.com>
2020-02-04 23:54:21 +0100

    Add objects and functions

---
+++ HorizontalniHitac/Objects/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorizontalniHitac.Objects
+{
+    class Ball
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Radius { get; set; }
+
+        public Ball()
+        {
+            X = 0;
+            Y = 0;
+            Radius = 30;
+        }
+
+        public void Move(float dX, float dY)
+        {
+            X += dX;
+            Y += dY;
+        }
+
+        public void Draw(System.Drawing.Graphics g)
+        {
+            g.DrawEllipse(new System.Drawing.Pen(System.Drawing.Color.Orange, 2), X, Y, Radius * 2, Radius * 2);
+            g.FillEllipse(new System.Drawing.SolidBrush(System.Drawing.Color.Orange), X, Y, Radius * 2, Radius * 2);
+        }
+    }
+}
---
+++ HorizontalniHitac/Objects/Target.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorizontalniHitac.Objects
+{
+    class Target
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Radius { get; set; }
+
+        public Target() { }
+        public Target(int x, int y)
+        {
+            Radius = 60;
+            Random rand = new Random();
+            X = rand.Next(x / 3, x - (int)Radius*2);
+            Y = y - (int) Radius;
+        }
+
+        public void Draw(System.Drawing.Graphics g)
+        {
+            g.DrawEllipse(new System.Drawing.Pen(System.Drawing.Color.Black, 4), X, Y, Radius * 2, Radius / 2);
+        }
+    }
+}
